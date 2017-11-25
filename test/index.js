2e867942139197aad5b36f2e83591061ca97ab8d
romanver777 <romanver777@yandex.ru>
2017-11-25 18:02:44 +0300

    commit

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ import { assert } from 'chai';
 import { randomNumberArray, randomStringArray, randomValue as random } from '../helper';
 import { calculator, isAllTrue, isSomeTrue, returnBadArguments } from '../src/index';
 
+
 describe('ДЗ 2 - работа с исключениями и отладчиком', () => {
     describe('isAllTrue', () => {
         it('должна вызывать fn для всех элементов массива', () => {
